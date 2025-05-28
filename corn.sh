cb9e1c1c5b046359c38ef58140b7630177294bdc
1577880969 <1577880969@qq.com>
2025-05-28 16:26:19 +0800

    m

--- corn.sh
+++ corn.sh
@@ -1,5 +1,5 @@
-*/1 * * * * curl -X POST -H "token:eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyX2lkIjo2NjYsImV4cCI6MjEwODI1NzE2MywiaXNzIjoiZ2luLWJsb2cifQ.6gyUwAAjObhSlseb5E84MFS1HWmeD8LaYuh1lFkEVvQ" -H "Content-Type: application/json" -d '{"folder":"INBOX","limit":100}' http://localhost:7080/api/v1/emails/list
+*/1 * * * * curl -X POST -H "token:eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyX2lkIjo2NjYsImV4cCI6MjEwODI1NzE2MywiaXNzIjoiZ2luLWJsb2cifQ.6gyUwAAjObhSlseb5E84MFS1HWmeD8LaYuh1lFkEVvQ" -H "Content-Type: application/json" -d '{"folder":"INBOX","limit":500}' http://localhost:7080/api/v1/emails/list
 
 
 
-*/2 * * * * curl -X POST -H "token:eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyX2lkIjo2NjYsImV4cCI6MjEwODI1NzE2MywiaXNzIjoiZ2luLWJsb2cifQ.6gyUwAAjObhSlseb5E84MFS1HWmeD8LaYuh1lFkEVvQ" -H "Content-Type: application/json" -d '{"limit":20}' http://localhost:7080/api/v1/emails/content
+*/2 * * * * curl -X POST -H "token:eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyX2lkIjo2NjYsImV4cCI6MjEwODI1NzE2MywiaXNzIjoiZ2luLWJsb2cifQ.6gyUwAAjObhSlseb5E84MFS1HWmeD8LaYuh1lFkEVvQ" -H "Content-Type: application/json" -d '{"limit":10}' http://localhost:7080/api/v1/emails/content
