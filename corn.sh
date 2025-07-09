12b987a1ca36dffb6e3c0434263ab93d9837e5c1
1577880969 <1577880969@qq.com>
2025-07-09 15:51:37 +0800

    m

--- corn.sh
+++ corn.sh
@@ -7,4 +7,4 @@
 
 
 
-*/3 * * * * curl -X POST -H "token:eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyX2lkIjo2NjYsImV4cCI6MjEwODI1NzE2MywiaXNzIjoiZ2luLWJsb2cifQ.6gyUwAAjObhSlseb5E84MFS1HWmeD8LaYuh1lFkEVvQ" -H "Content-Type: application/json" -d '{"limit":10}' http://localhost:7080/api/v1/emails/tr_send
+*/3 * * * * curl -X POST -H "token:eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyX2lkIjo2NjYsImV4cCI6MjEwODI1NzE2MywiaXNzIjoiZ2luLWJsb2cifQ.6gyUwAAjObhSlseb5E84MFS1HWmeD8LaYuh1lFkEVvQ" -H "Content-Type: application/json" -d '{"node": 1,"limit":10}' http://localhost:7080/api/v1/emails/tr_send
